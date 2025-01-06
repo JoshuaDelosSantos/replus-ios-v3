192a486be36be3cbfd3aa854a686f66d408e34d4
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-06 21:06:16 +1000

    Add initial code for initialisation

--- replus/ViewModels/SessionViewModel.swift
+++ replus/ViewModels/SessionViewModel.swift
@@ -5,3 +5,19 @@
 //  Created by Joshua Delos Santos on 6/1/2025.
 //
 
+
+import CoreData
+
+
+class SessionViewModel: ObservableObject {
+    
+    private let moc: NSManagedObjectContext
+    
+    init(moc: NSManagedObjectContext) {
+        self.moc = moc
+    }
+    
+    func fetchSessions() {
+        
+    }
+}
