8cc477ee828a71d30470136704a426ea39869958
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-06 21:11:43 +1000

    Add code for fetch request

--- replus/ViewModels/SessionViewModel.swift
+++ replus/ViewModels/SessionViewModel.swift
@@ -13,11 +13,23 @@ class SessionViewModel: ObservableObject {
     
     private let moc: NSManagedObjectContext
     
+    @Published var sessions: [Session] = []
+    
     init(moc: NSManagedObjectContext) {
         self.moc = moc
     }
     
     func fetchSessions() {
+        let request: NSFetchRequest<Session> = Session.fetchRequest()
         
+        do {
+            // Fetch sessions from core data
+            sessions = try moc.fetch(request)
+            print("Fetched \(sessions.count) sessions")
+            
+        } catch {
+            // Log Error
+            print("SessionViewModel: Failed to fetch sessions: \(error.localizedDescription)")
+        }
     }
 }
