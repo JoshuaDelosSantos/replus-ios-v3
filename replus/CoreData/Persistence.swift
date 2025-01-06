e411dc4911a59b341ca8c1aa1fa67f93a2386fa7
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-06 19:40:25 +1000

    Add initial code for data persistence with core data

--- replus/CoreData/Persistence.swift
+++ replus/CoreData/Persistence.swift
@@ -6,4 +6,18 @@
 //
 
 
+import CoreData
+import Foundation
 
+
+class PersistenceController: ObservableObject {
+    let container = NSPersistentContainer(name: "Replus")
+    
+    init () {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
