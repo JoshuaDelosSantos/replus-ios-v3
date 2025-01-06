f5413ed2d103f4b68b266b611287fc029d0209bc
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-06 19:21:51 +1000

    Initial Commit

---
+++ replus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  replus
+//
+//  Created by Joshua Delos Santos on 6/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ replus/replusApp.swift
@@ -0,0 +1,17 @@
+//
+//  replusApp.swift
+//  replus
+//
+//  Created by Joshua Delos Santos on 6/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct replusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
