94fc704c2b53ca4d4597764ab818eabe62a7c1a8
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-08 19:19:56 +1000

    Add code for initial card view

--- replus/Views/SessionCardView.swift
+++ replus/Views/SessionCardView.swift
@@ -5,3 +5,21 @@
 //  Created by Joshua Delos Santos on 8/1/2025.
 //
 
+
+
+import SwiftUI
+
+
+struct SessionCardView: View {
+    let session: Session
+    
+    var body: some View {
+        Text(session.name ?? "Unamed Session")
+            .font(.headline)
+            .padding()
+            .onAppear {
+                //Log
+                print("SessionCardView: Session ID: \(String(describing: session.id)), Name: \(session.name ?? "Unnamed")")
+            }
+    }
+}
