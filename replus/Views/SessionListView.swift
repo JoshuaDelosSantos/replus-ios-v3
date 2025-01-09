e5190cac749ad6cd82cf370c7c52b29c1bcc486c
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-09 15:27:04 +1000

    Add code to implement edit session functionality

--- replus/Views/SessionListView.swift
+++ replus/Views/SessionListView.swift
@@ -14,6 +14,8 @@ struct SessionListView: View {
     
     @StateObject private var viewModel: SessionViewModel
     @State private var isShowingAddSessionSheet: Bool = false
+    @State private var isEditing: Bool = false
+    @State private var isShowingEditSessionSheet: Bool = false
     
     init(viewModel: SessionViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
@@ -21,7 +23,7 @@ struct SessionListView: View {
     
     var body: some View {
         NavigationView {
-            displaySession()
+            displaySessions()
             .navigationTitle("Sessions")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
@@ -29,14 +31,23 @@ struct SessionListView: View {
                         Label("Add", systemImage: "plus")
                     }
                 }
+    
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {toggleEditMode()}) {
+                        Text(isEditing ? "Done" : "Edit")
+                    }
+                }
             }
             .sheet(isPresented: $isShowingAddSessionSheet) {
                 AddSessionView(viewModel: viewModel)
             }
+            .sheet(isPresented: $isShowingEditSessionSheet) {
+                EditSessionView(viewModel: viewModel)
+            }
         }
     }
     
-    private func displaySession() -> some View {
+    private func displaySessions() -> some View {
         VStack {
             // No sessions
             if viewModel.sessions.isEmpty {
@@ -45,9 +56,26 @@ struct SessionListView: View {
                     .padding()
             } else {
                 List(viewModel.sessions) { session in
-                    SessionCardView(session: session)
+                    HStack {
+                        if isEditing {
+                            displayRenameButton()
+                        }
+                        SessionCardView(session: session)
+                    }
                 }
             }
+        }
+    }
+    
+    private func toggleEditMode() {
+            isEditing.toggle()
+    }
+    
+    private func displayRenameButton() -> some View {
+        Button(action: {isShowingEditSessionSheet = true}) {
+            Image(systemName: "pencil")
+                .foregroundColor(.blue)
+                .imageScale(.large)
         }
     }
     
