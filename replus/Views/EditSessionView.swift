2c29264c1f345cec137a50cd38b0f0cd1d9fc060
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-09 15:22:51 +1000

    Add initial edit session view

--- replus/Views/EditSessionView.swift
+++ replus/Views/EditSessionView.swift
@@ -5,3 +5,30 @@
 //  Created by Joshua Delos Santos on 9/1/2025.
 //
 
+
+import SwiftUI
+import CoreData
+
+
+struct EditSessionView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @StateObject private var viewModel: SessionViewModel
+    @State private var sessionName: String = ""
+    
+    init(viewModel: SessionViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
+    var body: some View {
+        Text("Edit")
+    }
+    
+}
+
+
+#Preview {
+    let persistenceController = PersistenceController(inMemory: true)
+    let viewModel = SessionViewModel(moc: persistenceController.container.viewContext)
+    
+    EditSessionView(viewModel: viewModel)
+}
