2052d44096721bdfe48f7ab7f739d5ceacad6d42
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-10 21:29:27 +1000

    Update code to initialise variable using viewModel function

--- replus/Views/EditSessionView.swift
+++ replus/Views/EditSessionView.swift
@@ -13,21 +13,23 @@ import CoreData
 struct EditSessionView: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject private var viewModel: SessionViewModel
+    @State private var selectedSessionName: String = ""
     @State private var newSessionName: String = ""
     @State private var isSaving = false
     
     init(viewModel: SessionViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
+        self.selectedSessionName = viewModel.getSelectedSessionName()!
     }
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Edit Session")) {
+                Section(header: Text("Rename session")) {
                     TextField("New Session Name", text: $newSessionName)
                 }
             }
-            .navigationTitle("Edit Session")
+            .navigationTitle("Editing \(self.selectedSessionName) session")
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button("Cancel") {
