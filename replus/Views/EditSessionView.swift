ab2be79f8fd2b8adeef307f16870319381f90209
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-10 19:12:52 +1000

    Add initial code for EditSessionView

--- replus/Views/EditSessionView.swift
+++ replus/Views/EditSessionView.swift
@@ -13,22 +13,45 @@ import CoreData
 struct EditSessionView: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject private var viewModel: SessionViewModel
-    @State private var sessionName: String = ""
+    @State private var newSessionName: String = ""
+    @State private var isSaving = false
     
     init(viewModel: SessionViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
     
     var body: some View {
-        Text("Edit")
+        NavigationView {
+            Form {
+                Section(header: Text("Edit Session")) {
+                    TextField("New Session Name", text: $newSessionName)
+                }
+            }
+            .navigationTitle("Edit Session")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        Task {await updateSession()}
+                    }
+                    .disabled(newSessionName.isEmpty || isSaving)
+                }
+            }
+        }
     }
     
-}
-
-
-#Preview {
-    let persistenceController = PersistenceController(inMemory: true)
-    let viewModel = SessionViewModel(moc: persistenceController.container.viewContext)
+    private func updateSession() async {
+        isSaving = true
+        defer { isSaving = false}
+        
+        viewModel.updateSession(newName: newSessionName)
+        print("EditSessionView: Updated session")  // Log
+        
+        presentationMode.wrappedValue.dismiss()
+    }
     
-    EditSessionView(viewModel: viewModel)
 }
