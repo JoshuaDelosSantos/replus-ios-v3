e102c4b62004f39ec1f28ac70fbbd70b924f43a9
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-19 14:48:55 +1000

    Add properties for testing

--- ReplusTests/SessionViewModelTests.swift
+++ ReplusTests/SessionViewModelTests.swift
@@ -7,8 +7,14 @@
 
 
 import XCTest
+import CoreData
+@testable import replus
 
-final class SessionViewModelTests:XCTestCase {
+
+final class SessionViewModelTests: XCTestCase {
     
-    
+    // Properties
+    var viewModel: SessionViewModel!
+    var persistentContainer: NSPersistentContainer!
+    var moc: NSManagedObjectContext!
 }
