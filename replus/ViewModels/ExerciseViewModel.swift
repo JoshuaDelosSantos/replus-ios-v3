0bd827c7ea16e9d36efff26e1f36266a09caa190
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-01-21 15:57:51 +1000

    Add initial code

--- replus/ViewModels/ExerciseViewModel.swift
+++ replus/ViewModels/ExerciseViewModel.swift
@@ -5,3 +5,28 @@
 //  Created by Joshua Delos Santos on 21/1/2025.
 //
 
+
+import CoreData
+
+
+class ExerciseViewModel: ObservableObject {
+    
+    private let moc: NSManagedObjectContext
+    
+    @Published var session: Session?
+    @Published var exercises: [Exercise] = []
+    
+    init(moc: NSManagedObjectContext) {
+        self.moc = moc
+    }
+    
+    
+    
+    private func saveContext() throws {
+        if moc.hasChanges {
+            print("SessionViewModel: Saving changes...")  // Log
+            try moc.save()
+            print("SessionViewModel: Changes saved")  // Log
+        }
+    }
+}
